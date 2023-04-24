9acf5d53f8ba058353331a8fd14f87ecf50e8071
Drioskarii <kagekrigeren@gmail.com>
2023-04-24 10:12:17 +0200

    Initial Commit
    Thanks GPT for doing 99% of the work lol

---
+++ SteamWorkAround.ps1
@@ -0,0 +1,21 @@
+﻿$processName = "steamwebhelper"
+$process = Get-Process $processName
+
+if ($process) {
+     Write-Host "The process $processName is currently running."
+    $process.Kill()
+
+    Write-Host "The process tree for $processName has been terminated."
+    $process = Get-Process -Name $processName -ErrorAction SilentlyContinue
+
+    #It opens so fast that this can't keep up
+#    if (!$process) {
+#        Write-Host "The process $processName has been removed from Task Manager."
+#    }
+#    else {
+#        Write-Host "The process $processName could not be removed from Task Manager."
+#    }
+}
+else {
+    Write-Host "The process $processName is not currently running."
+}
